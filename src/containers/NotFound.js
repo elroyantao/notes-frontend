b56d8da73b8ddf719c44109d9c64a0f6f7ef7d9c
Elroy Antao <elroyantao@gmail.com>
2020-02-13 00:53:24 +0000

    not found route

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './NotFound.module.css'
+
+export default function NotFound () {
+  return (
+    <div className={styles['NotFound']}>
+      <h3>Sorry, page not found!</h3>
+    </div>
+  )
+}
