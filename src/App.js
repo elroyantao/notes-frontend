c665880b17316aab48dfe4b807bc271907e6d629
Elroy Antao <elroyantao@gmail.com>
2020-02-13 00:31:54 +0000

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
 
-function App() {
+import Routes from './Routes'
+import styles from './App.module.css'
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${styles['App']} container`}>
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand className={styles['navbar-brand']}>
+            <Link to='/'>Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
